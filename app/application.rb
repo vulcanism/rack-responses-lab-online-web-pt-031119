e3777d97e8972a8d3efd5b8627369842c79d73ec
Savannah C <sgcorrell@gmail.com>
2019-06-05 16:04:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now.to_i < 12
+    time = Time.now
+    
+    if time < 12
       resp.write "Good Morning!"
     elsif
       resp.write "Good Afternoon!"
